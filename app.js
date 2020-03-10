3464f1072c18fec6d04a7e04b2ae2b350a007ee7
iean <ieanhennig8@gmail.com>
2020-03-09 20:18:15 -0500

    added in moment js

---
+++ app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+var momentTime = moment().format("MMMM Do YYYY, h: mm:ss a");
+
+var displayMom = $("#currentDay");
+
+displayMom.text(momentTime);
+
+
+
+
+
+
+
+
+
+});
